341a9adf5145ebb9c26a568967d17678563045e8
Sean MacLennan <git@seanm.ca>
2013-11-19 00:44:54 -0500

    Now that we display the actual value of the variables we don't need to mention the defaults.

--- vars-array.h
+++ vars-array.h
@@ -4,11 +4,11 @@
 struct avar Vars[] = {
 	{ "add-nl",		FLAG,		{1},
 	  "If this flag variable is set, the editor will insure that "
-	  "all saved files end in a Newline. The default is on."
+	  "all saved files end in a Newline."
 	},
 	{ "backup",		FLAG,		{0},
 	  "If this flag variable is set, a backup file is created when "
-	  "a file is written. The default is off."
+	  "a file is written."
 	},
 	{ "bell",		FLAG,		{1},
 	  "If this variable is set, the bell will flash the screen."
@@ -21,22 +21,20 @@ struct avar Vars[] = {
 	  "A tab is a single character that is displayed as one or more "
 	  "spaces on the screen. This variable defines the number of "
 	  "spaces displayed per tab in C mode buffers, even if the buffer "
-	  "is in View mode. The default is 8 spaces. See also Tabs."
+	  "is in View mode. See also tabs."
 	},
 	{ "comments",		FLAG,		{1},
 	  "If set then Zedit tries to bold (display in red) "
-	  "comments in C and shell mode buffers. Enabled by default. "
+	  "comments in C and shell mode buffers."
 	},
 	{ "exact",		FLAG,		{1},
 	  "If Exact is set, all searches and replaces are case "
 	  "sensitive. This means that the search string 'string' will "
-	  "only match 'string', not 'STRING', or 'String'. The default "
-	  "is on."
+	  "only match 'string', not 'STRING', or 'String'."
 	},
 	{ "fill-ch-width",	DECIMAL,	{72},
 	  "This is the column at which the Text mode commands wrap the "
 	  "right margin. It is also used by the Center Line command. "
-	  "The default is 72."
 	},
 	{ "grep",		STRING,		{0},
 	  "The command to use for the Zedit grep command. The default "
@@ -45,8 +43,7 @@ struct avar Vars[] = {
 	{ "lines",		FLAG,		{0},
 	  "This flag variable turns on a line:column indicator on the "
 	  "mode line. The Lines feature can cause a lot of cursor "
-	  "movement and is irritating on some slow terminals. The "
-	  "default is off."
+	  "movement and is irritating on some slow terminals."
 	},
 	{ "make-cmd",		STRING,		{0},
 	  "This is a string variable which contains the default string "
@@ -55,7 +52,7 @@ struct avar Vars[] = {
 	{ "normal",		FLAG,		{1},
 	  "If on, sets the default buffer mode to Normal. If off, sets "
 	  "the default buffer mode to Text. Some file extensions "
-	  "override this variable. The default is on."
+	  "override this variable."
 	},
 	{ "sh-extends",		STRING,		{0},
 	  "This variable defines up to 8 extensions that turn on Shell "
@@ -66,13 +63,13 @@ struct avar Vars[] = {
 	  "single ASCII 9 character. If the SpaceTabs variable is set, "
 	  "then a tab is inserted as spaces in the buffer. This flag "
 	  "does not affect tabs already in the buffer or tabs inserted "
-	  "in the PAW. The default is off."
+	  "in the PAW."
 	},
 	{ "tabs",		DECIMAL,	{8},
 	  "A tab is a single character that is displayed as one or more "
 	  "spaces on the screen. This variable defines the number of "
 	  "space characters displayed per tab in buffers that are not in "
-	  "a program mode. The default is 8 spaces per tab."
+	  "a program mode."
 	},
 	{ "t-extends",		STRING,		{0},
 	  "This variable defines up to 8 extensions that turn on Text "

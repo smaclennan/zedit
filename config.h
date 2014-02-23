d59ba5e56ad614928b47bf65a718e879b1a1d39e
Sean MacLennan <seanm@seanm.ca>
2014-02-22 19:12:29 -0500

    Let's have a config.h that works with DOS.

--- config.h
+++ config.h
@@ -1,9 +1,13 @@
 /* config.h - Zedit user configurable defines */
-#ifndef WIN32
+
+#if defined(DOS)
+#define INT_IS_16BITS	1
+#elif defined(WIN32)
+#define SHELL		1
+#else
 #define HAVE_TERMIOS	1
 #define ZLIB		1	/* Requires zlib -lz */
 #define SHELL		1
 #define SPELL		1	/* Requires libaspell */
 #define UNDO		0	/* EXPERIMENTAL */
 #endif
-

5affd0b64bd1df205d4af7bf239018b86f8f2a48
Sean MacLennan <seanm@seanm.ca>
2014-02-22 21:44:16 -0500

    More keys... more keys.

--- win32/doskbd.c
+++ win32/doskbd.c
@@ -24,13 +24,18 @@
 int Cmdpushed = -1;
 static int kb_hit;
 
-#if 0
 static int alts[] = {
-	/* 10 */ 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', 0, 0,
-	/* 1E */ 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 0, 0,
-	/* 2C */ 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/'
-	/* 36 */ };
-#endif
+	/* 10 */ 'Q' + 128, 'W' + 128, 'E' + 128, 'R' + 128, 'T' + 128, 'Y' + 128,
+	/* 16 */ 'U' + 128, 'I' + 128, 'O' + 128, 'P' + 128, '[' + 128, ']' + 128, 0, 0,
+	/* 1E */ 'A' + 128, 'S' + 128, 'D' + 128, 'F' + 128, 'G' + 128, 'H' + 128,
+	/* 24 */ 'J' + 128, 'K' + 128, 'L' + 128, ';' + 128, '\'' + 128, 0, 0, 0,
+	/* 2C */ 'Z' + 128, 'X' + 128, 'C' + 128, 'V' + 128, 'B' + 128, 'N' + 128,
+	/* 32 */ 'M' + 128, ',' + 128, '.' + 128, '/', 0, 0, 0, 0, 0,
+	/* 3B */ TC_F1, TC_F2, TC_F3, TC_F4, TC_F5, TC_F6, TC_F7, TC_F8, TC_F9, TC_F10, 0, 0,
+	/* 47 */ TC_HOME, TC_UP, TC_PGUP, 0,
+	/* 4B */ TC_LEFT, 0, TC_RIGHT, 0, TC_END,
+	/* 50 */ TC_DOWN, TC_PGDOWN, TC_INSERT, TC_DELETE,
+};
 
 int tgetcmd(void)
 {
@@ -46,19 +51,9 @@ int tgetcmd(void)
 	cmd = getch() & 0x7f;
 	if (cmd == 0) {
 		cmd = getch();
-		if (cmd >= 0x3b && cmd <= 0x44)
-			return TC_F1 + cmd - 0x3B;
+		if (cmd >= 0x10 && cmd <= 0x53)
+			return alts[cmd - 0x10];
 		switch (cmd) {
-		case 0x47: return TC_HOME;
-		case 0x48: return TC_UP;
-		case 0x49: return TC_PGUP;
-		case 0x4B: return TC_LEFT;
-		case 0x4D: return TC_RIGHT;
-		case 0x4F: return TC_END;
-		case 0x50: return TC_DOWN;
-		case 0x51: return TC_PGDOWN;
-		case 0x52: return TC_INSERT;
-		case 0x53: return TC_DELETE;
 		case 0x85: return TC_F11;
 		case 0x86: return TC_F12;
 		default:   return TC_UNKNOWN;

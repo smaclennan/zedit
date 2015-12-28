d7e09e54421b5f08adc7a246e8c5f6f7e196da53
Sean MacLennan <seanm@seanm.ca>
2015-12-27 20:23:06 -0500

    More docs.

--- buff.c
+++ buff.c
@@ -35,6 +35,10 @@ void undo_clear(struct buff *buff);
 #endif
 
 static void dummy_bsetmod(struct buff *buff) {}
+/** If you need to know when the buffer is modified, this callback
+ * will get called to notify you. By default it is set to a dummy
+ * function that does nothing.
+ */
 void (*bsetmod)(struct buff *buff) = dummy_bsetmod;
 
 int NumBuffs; /**< Current number of buffers. Not thread safe. */

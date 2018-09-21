3527a545117351789a874050ec3f697862b1fa0f
Sean MacLennan <seanm@seanm.ca>
2018-09-20 20:11:24 -0400

    Share strfmt.c with samlib.

--- buff/binstr.c
+++ buff/binstr.c
@@ -26,108 +26,8 @@
 */
 
 /* \cond skip */
-struct outbuff {
-	struct buff *buff;
-	char *str;
-	int len;
-};
-
-/** Helper function for binstr(). */
-static int outchar(struct outbuff *out, char ch)
-{
-	if (out->buff)
-		return binsert(out->buff, ch);
-	if (out->len > 1) {
-		*out->str++ = ch;
-		--out->len;
-		return 1;
-	}
-	return 0;
-}
-
-/** Helper function for binstr().
- * @param fmt The format string.
- * @param[out] saw_neg For strings, 1 if we want to left justify.
- * @param[out] len The width field if set.
- * @param[out] n Number of bytes of fmt consumed.
- */
-static char valid_format(const char *fmt, int *saw_neg, int *len, int *n)
-{
-	*saw_neg = *len = *n = 0;
-	++fmt; ++*n; /* skip % */
-	if (*fmt == '-') {
-		*saw_neg = 1;
-		++fmt; ++*n;
-	}
-	while (isdigit(*fmt)) {
-		*len = *len * 10 + *fmt - '0';
-		++fmt; ++*n;
-	}
-	if (*fmt != 's')
-		*saw_neg = 0;
-	return *fmt;
-}
-
-/** Helper function for binstr(). */
-static int out_str(struct outbuff *out, const char *s, int saw_neg, int len)
-{
-	int slen = strlen(s);
-
-	if (saw_neg == 0)
-		while (slen++ < len)
-			if (!outchar(out, ' '))
-				return 0;
-	while (*s)
-		if (!outchar(out, *s++))
-			return 0;
-	while (slen++ < len)
-		if (!outchar(out, ' '))
-			return 0;
-	return 1;
-}
-
-/** Helper function for binstr(). */
-static int handle_format(struct outbuff *out, const char **fmt, va_list ap)
-{
-	char tmp[22];
-	int saw_neg, len, n;
-
-	switch (valid_format(*fmt, &saw_neg, &len, &n)) {
-	case 's':
-		*fmt += n;
-		return out_str(out, va_arg(ap, char *), saw_neg, len);
-	case 'd':
-		*fmt += n;
-		bitoa(va_arg(ap, int), tmp);
-		return out_str(out, tmp, saw_neg, len);
-	case 'u':
-		*fmt += n;
-		butoa(va_arg(ap, unsigned), tmp);
-		return out_str(out, tmp, saw_neg, len);
-	case 'x':
-		*fmt += n;
-		bxtoa(va_arg(ap, unsigned), tmp);
-		return out_str(out, tmp, saw_neg, len);
-	case 'l':
-		switch (*(*fmt + n + 1)) {
-		case 'd':
-			*fmt += n + 1;
-			bitoa(va_arg(ap, long), tmp);
-			return out_str(out, tmp, saw_neg, len);
-		case 'u':
-			*fmt += n + 1;
-			butoa(va_arg(ap, unsigned long), tmp);
-			return out_str(out, tmp, saw_neg, len);
-		case 'x':
-			*fmt += n + 1;
-			bxtoa(va_arg(ap, unsigned long), tmp);
-			return out_str(out, tmp, saw_neg, len);
-		}
-		/* fall through */
-	default:
-		return outchar(out, **fmt);
-	}
-}
+#define BUFFERED
+#include "strfmt.c"
 /* \endcond */
 
 /** Insert a string formatted with variable arguments into a buffer.
@@ -142,79 +42,13 @@ static int handle_format(struct outbuff *out, const char **fmt, va_list ap)
  */
 int binstr(struct buff *buff, const char *fmt, ...)
 {
-	struct outbuff out;
+	struct outbuff out = { .buff = buff, .n = 0 };
 	va_list ap;
-	int rc = 1;
-
-	out.buff = buff;
 
 	va_start(ap, fmt);
-	while (*fmt && rc) {
-		if (*fmt == '%')
-			rc = handle_format(&out, &fmt, ap);
-		else
-			rc = binsert(buff, *fmt);
-		++fmt;
-	}
+	int rc = __strfmt(&out, fmt, ap);
 	va_end(ap);
 
 	return rc;
-}
-
-/** Lower level interface to strfmt() when you already have the va_list.
- * @param str The output string.
- * @param len The length of the output string.
- * @param fmt The output format.
- * @param ap The va_list.
- * @return The number of bytes inserted.
- */
-int strfmt_ap(char *str, int len, const char *fmt, va_list ap)
-{
-	struct outbuff out;
-	int rc = 1;
-
-	if (len < 1)
-		return 0;
-
-	out.buff = NULL;
-	out.str = str;
-	out.len = len;
-
-	while (*fmt && rc) {
-		if (*fmt == '%')
-			rc = handle_format(&out, &fmt, ap);
-		else
-			rc = outchar(&out, *fmt);
-		++fmt;
-	}
-
-	/* We leave room for the NULL */
-	*out.str = 0;
-
-	return len - out.len;
-}
-
-/** Poor man's snprintf.
- *
- * Supports a subset of printf: %%s, %%d, %%u. Format can contain a width
- * and a minus (-) for left justify.
- *
- * @param str The output string.
- * @param len The length of the output string.
- * @param fmt The output format.
- * @param ... The zero or more arguments.
- * @return The number of bytes inserted.
- */
-int strfmt(char *str, int len, const char *fmt, ...)
-{
-	if (len < 1)
-		return 0;
-
-	va_list ap;
-	va_start(ap, fmt);
-	int n = strfmt_ap(str, len, fmt, ap);
-	va_end(ap);
-
-	return n;
 }
 /* @} */
---
+++ buff/strfmt.c
@@ -0,0 +1,299 @@
+#include <stdarg.h>
+#include <ctype.h>
+#include "samlib.h"
+
+#define WIDTH_MASK		0x0fffff
+#define SAW_NEG			0x100000
+#define SAW_ZERO		0x200000
+#define SAW_LONG		0x400000
+
+struct outbuff {
+#ifdef BUFFERED
+	struct buff *buff;
+#endif
+	char *str;
+	int len;
+	int n;
+};
+
+static void reverse_string(char *start, char *end)
+{
+	int len = (end - start) / 2;
+	--end;
+	for (int i = 0; i < len; ++i) {
+		char temp = *end;
+		*end = *start;
+		*start = temp;
+		start++;
+		end--;
+	}
+}
+
+/** Integer to ascii.
+ * @param val The integer to convert.
+ * @param out The output string.
+ * @return A pointer to out.
+ */
+char *int2str(long val, char *out)
+{
+	if (val == 0)
+		strcpy(out, "0");
+	else {
+		char *p = out;
+		int neg = val < 0;
+
+		if (neg)
+			val = -val;
+
+		while (val > 0) {
+			*p++ = (val % 10) + '0';
+			val /= 10;
+		}
+		if (neg) *p++ = '-';
+		*p = 0;
+
+		reverse_string(out, p);
+	}
+
+	return out;
+}
+
+/** Unsigned integer to ascii.
+ * @param val The integer to convert.
+ * @param out The output string.
+ * @return A pointer to out.
+ */
+char *uint2str(unsigned long val, char *out)
+{
+	if (val == 0)
+		strcpy(out, "0");
+	else {
+		char *p = out;
+
+		while (val > 0) {
+			*p++ = (val % 10) + '0';
+			val /= 10;
+		}
+		*p = 0;
+
+		reverse_string(out, p);
+	}
+	return out;
+}
+
+/** Hex to ascii.
+ * @param val The integer to convert.
+ * @param out The output string.
+ * @return A pointer to out.
+ */
+char *hex2str(unsigned long val, char *out)
+{
+	if (val == 0)
+		strcpy(out, "0");
+	else {
+		char *p = out;
+
+		while (val > 0) {
+			*p++ = tohex[val & 0xf];
+			val >>= 4;
+		}
+		*p = 0;
+
+		reverse_string(out, p);
+	}
+	return out;
+}
+
+static void outmemset(struct outbuff *out, char c, int size)
+{
+	if (size > 0) {
+		out->n += size;
+#ifdef BUFFERED
+		if (out->buff) {
+			for (int i = 0; i < size; ++i)
+				binsert(out->buff, c);
+			return;
+		}
+#endif
+		size = MIN(size, out->len);
+		if (size > 0) {
+			memset(out->str, c, size);
+			out->str += size;
+			out->len -= size;
+		}
+	}
+}
+
+static void outmemcpy(struct outbuff *out, const char *str, int size)
+{
+	if (size > 0) {
+		out->n += size;
+#ifdef BUFFERED
+		if (out->buff) {
+			for (int i = 0; i < size; ++i)
+				binsert(out->buff, str[i]);
+			return;
+		}
+#endif
+		size = MIN(size, out->len);
+		if (size > 0) {
+			memcpy(out->str, str, size);
+			out->str += size;
+			out->len -= size;
+		}
+	}
+}
+
+static void outchar(struct outbuff *out, char ch)
+{
+	out->n++;
+#ifdef BUFFERED
+	if (out->buff) {
+		binsert(out->buff, ch);
+		return;
+	}
+#endif
+	if (out->len > 0) {
+		*out->str++ = ch;
+		--out->len;
+	}
+}
+
+static void outstr(struct outbuff *out, const char *s, unsigned flags)
+{
+	int slen = strlen(s);
+	int len = flags & WIDTH_MASK;
+	int pad = len - slen;
+
+	if ((flags & SAW_NEG) == 0) {
+		outmemset(out, ' ', pad);
+		pad = 0;
+	}
+	outmemcpy(out, s, slen);
+	outmemset(out, ' ', pad);
+}
+
+static void outnum(struct outbuff *out, const char *s, unsigned flags)
+{
+	int slen = strlen(s);
+	int len = flags & WIDTH_MASK;
+
+	if (len)
+		outmemset(out, (flags & SAW_ZERO) ? '0' : ' ', len - slen);
+
+	outmemcpy(out, s, slen);
+}
+
+/** Lower level interface to strfmt() when you already have the va_list.
+ * @param str The output string.
+ * @param len The length of the output string.
+ * @param fmt The output format.
+ * @param ap The va_list.
+ * @return The number of bytes in the source string (like snprintf).
+ */
+static int __strfmt(struct outbuff *out, const char *fmt, va_list ap)
+{
+	char tmp[22];
+
+	while (*fmt) {
+		if (*fmt == '%') {
+			const char *save = fmt++;
+			unsigned flags = 0, width = 0;
+
+			if (*fmt == '-') {
+				++fmt;
+				flags |= SAW_NEG;
+			}
+			if (*fmt == '0')
+				flags |= SAW_ZERO;
+			while (isdigit(*fmt)) {
+				width = width * 10 + *fmt - '0';
+				++fmt;
+			}
+			flags |= width & WIDTH_MASK;
+			if (*fmt == 'l') {
+				++fmt;
+				flags |= SAW_LONG;
+			}
+			switch (*fmt) {
+			case 's':
+				outstr(out, va_arg(ap, char *), flags);
+				break;
+			case 'c':
+				// char is promoted to int in ...
+				outchar(out, va_arg(ap, int));
+				break;
+			case 'd':
+				if (flags & SAW_LONG)
+					int2str(va_arg(ap, long), tmp);
+				else
+					int2str(va_arg(ap, int), tmp);
+				outnum(out, tmp, flags);
+				break;
+			case 'u':
+				if (flags & SAW_LONG)
+					uint2str(va_arg(ap, unsigned long), tmp);
+				else
+					uint2str(va_arg(ap, unsigned), tmp);
+				outnum(out, tmp, flags);
+				break;
+			case 'x':
+				if (flags & SAW_LONG)
+					hex2str(va_arg(ap, unsigned long), tmp);
+				else
+					hex2str(va_arg(ap, unsigned), tmp);
+				outnum(out, tmp, flags);
+				break;
+			default:
+				outchar(out, '%');
+				fmt = save;
+			}
+		} else
+			outchar(out, *fmt);
+		++fmt;
+	}
+
+	/* We leave room for the NULL */
+	*out->str = 0;
+
+	return out->n;
+}
+
+/** Lower level interface to strfmt() when you already have the va_list.
+ * @param str The output string.
+ * @param len The length of the output string.
+ * @param fmt The output format.
+ * @param ap The va_list.
+ * @return The number of bytes in the source string (like snprintf).
+ */
+int strfmt_ap(char *str, int len, const char *fmt, va_list ap)
+{
+	struct outbuff out = { .str = str, .len = len - 1, .n = 0 };
+
+	if (len < 1)
+		return 0;
+
+	return __strfmt(&out, fmt, ap);
+}
+
+/** Poor man's snprintf.
+ *
+ * Supports a subset of printf: %s, %c, %d, %u, %x, %l[dux]. Format can
+ * contain a width and a minus (-) for left justify. Numbers starting
+ * with 0 and a width are zero padded.
+ *
+ * @param str The output string.
+ * @param len The length of the output string.
+ * @param fmt The output format.
+ * @param ... The zero or more arguments.
+ * @return The number of bytes in the source (like snprintf).
+ */
+int strfmt(char *str, int len, const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	int n = strfmt_ap(str, len, fmt, ap);
+	va_end(ap);
+
+	return n;
+}

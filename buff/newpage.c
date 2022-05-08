6ecb0055f381ce1af71adc10f06b94f0e8ac1055
Sean MacLennan <seanm@seanm.ca>
2022-05-08 12:57:01 -0400

    Actually add the new file

---
+++ buff/newpage.c
@@ -0,0 +1,48 @@
+/* create a new page
+ * Copyright (C) 1988-2018 Sean MacLennan <seanm@seanm.ca>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this project; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "buff.h"
+
+/** @addtogroup buffer
+ * @{
+ */
+
+/** Low-level page function to create a new memory page and link into
+ * chain after curpage.
+ * @param buf The buffer to add the page to.
+ * @return The new page or NULL.
+ */
+struct page *newpage(struct buff *buff)
+{
+	struct page *page = (struct page *)calloc(1, sizeof(struct page));
+
+	if (page) {
+		page->prevp = buff->curpage;
+		if (buff->curpage->nextp) {
+			page->nextp = buff->curpage->nextp;
+			buff->curpage->nextp->prevp = page;
+		} else
+			buff->lastp = page;
+		buff->curpage->nextp = page;
+	}
+
+	return page;
+}
+
+/* @} */
